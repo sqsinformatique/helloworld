2ac8b4d22b6e4bac88b6430c2f7beaf2c0d11d07
Sergey Soldatov <sasolda3@mts.ru>
2020-06-06 14:12:20 +0300

    Updates

---
+++ precache-manifest.5af392a966b80197bbc6a3c16aae81d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7ccbb01d058de397315c1619b2460e",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "6d234167dab63c806d39",
+    "url": "/helloworld/static/css/2.ad8c1e94.chunk.css"
+  },
+  {
+    "revision": "122c095f6f783f5f8c6a",
+    "url": "/helloworld/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6d234167dab63c806d39",
+    "url": "/helloworld/static/js/2.ad94d6ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld/static/js/2.ad94d6ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122c095f6f783f5f8c6a",
+    "url": "/helloworld/static/js/main.b73cd306.chunk.js"
+  },
+  {
+    "revision": "430ef0e9846e44743734",
+    "url": "/helloworld/static/js/runtime-main.1d336961.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/helloworld/static/media/persik.4e1ec840.png"
+  }
+]);
