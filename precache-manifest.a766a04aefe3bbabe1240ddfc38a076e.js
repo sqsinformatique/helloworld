973d5df0d08ce7f5ed40875a526a13b9e268a17d
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2020-06-09 02:41:31 +0500

    Updates

---
+++ precache-manifest.a766a04aefe3bbabe1240ddfc38a076e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61275677a13b9ef4df4f4feeafceb823",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "2d4d626b4f30ee2413ad",
+    "url": "/helloworld/static/css/2.4911f66b.chunk.css"
+  },
+  {
+    "revision": "03256e90250a889487e1",
+    "url": "/helloworld/static/css/main.7dc4706b.chunk.css"
+  },
+  {
+    "revision": "2d4d626b4f30ee2413ad",
+    "url": "/helloworld/static/js/2.ded0eab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloworld/static/js/2.ded0eab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03256e90250a889487e1",
+    "url": "/helloworld/static/js/main.bc45a99a.chunk.js"
+  },
+  {
+    "revision": "430ef0e9846e44743734",
+    "url": "/helloworld/static/js/runtime-main.1d336961.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/helloworld/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/helloworld/precache-manifest.f6391943a34ce1b5d18bc1b8d02f0e6a.js"
+  "/helloworld/precache-manifest.a766a04aefe3bbabe1240ddfc38a076e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/helloworld/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
